da0c0e152778094fee7ef1071f55ec0156183a6c
Veronica Celseti <veronicacelseti12@gmail.com>
2020-06-30 13:47:13 +0200

    preparazione file blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>@yeld('page-title')</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        @include('elements.navbar')
+        @yield('content')
+        @include('elements.footer')
+         <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function () {
+    return view('home');
+})->name('homepage');
+
+Route::get('/faq', function () {
+    return view('faq');
+})->name('faq');
+
+Route::get('/privacy-policy', function () {
+    return view('privacy-policy');
+})->name('privacy-policy');
